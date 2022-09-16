ea13c65393fa37f8b7bd0e5c98a39fe94a58b6e2
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-16 19:25:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/UserMenu/App';
 import { store, persistor } from 'redux/store';
-// import { GoogleOAuthProvider } from '@moeindana/google-oauth';
+import { GoogleOAuthProvider } from '@moeindana/google-oauth';
 import './index.css';
 
 // console.log('process.env: ', process.env);
@@ -14,11 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                {/* <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}> */}
-                <BrowserRouter basename="/finance-counter">
-                    <App />
-                </BrowserRouter>
-                {/* </GoogleOAuthProvider> */}
+                <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+                    <BrowserRouter basename="/finance-counter">
+                        <App />
+                    </BrowserRouter>
+                </GoogleOAuthProvider>
             </PersistGate>
         </Provider>
     </React.StrictMode>
