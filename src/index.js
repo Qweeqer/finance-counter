5d511ac4fb82971654178132154fb1e80a28806e
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-16 18:51:32 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/UserMenu/App';
 import { store, persistor } from 'redux/store';
-import { GoogleOAuthProvider } from '@moeindana/google-oauth';
+// import { GoogleOAuthProvider } from '@moeindana/google-oauth';
 import './index.css';
 
-console.log('process.env: ', process.env);
-console.log('process.env.REACT_APP_CLIENT_ID: ', process.env.REACT_APP_CLIENT_ID);
+// console.log('process.env: ', process.env);
+// console.log('process.env.REACT_APP_CLIENT_ID: ', process.env.REACT_APP_CLIENT_ID);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </GoogleOAuthProvider>
+                {/* <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}> */}
+                <BrowserRouter basename="/finance-counter">
+                    <App />
+                </BrowserRouter>
+                {/* </GoogleOAuthProvider> */}
             </PersistGate>
         </Provider>
     </React.StrictMode>
